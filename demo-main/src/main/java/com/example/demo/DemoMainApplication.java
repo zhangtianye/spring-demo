0c86a344f314e389f683ecfd491e4e627904de6a
tianye <zhang_tian_ye@163.com>
2020-05-14 01:00:10 +0800

    SLF4J Log

--- demo-main/src/main/java/com/example/demo/DemoMainApplication.java
+++ demo-main/src/main/java/com/example/demo/DemoMainApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoMainApplication {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) {
         SpringApplication.run(DemoMainApplication.class, args);
--- demo-service/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ demo-service/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -4,14 +4,20 @@ import com.example.demo.api.UserService;
 import com.example.demo.domain.User;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class UserServiceImpl implements UserService {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public String getUserName(String name) {
         User user = User.builder()
                 .name(name)
                 .build();
+        logger.info(name);
         return user.getName();
     }
 }
